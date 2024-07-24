ed398421daddc45510c3a738a558b37f26736184
Dopezapha <officialnwaneridaniel@gmail.com>
2024-07-25 00:38:12 +1000

    Using prototype to resolve all promises and log body firstName lastName to the console

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
+}
