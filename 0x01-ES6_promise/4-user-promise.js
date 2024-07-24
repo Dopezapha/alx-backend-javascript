fec440e4533904c9edb386bc395a1aee0cac493f
Dopezapha <officialnwaneridaniel@gmail.com>
2024-07-25 01:15:06 +1000

    Using a prototype to return a resolved promise with an object

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName,
+  });
+}
