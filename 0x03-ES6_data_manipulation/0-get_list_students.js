587dd652fc6d26e261cb296fd890ff64d72bb61a
Dopezapha <officialnwaneridaniel@gmail.com>
2024-07-31 02:49:49 +1000

    Added Readme file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
-function getListStudents() {
+function ngetListStudents() {
     return [
-	    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-            {id: 2, firstName: 'James', location: 'Columbia'},
-	    {id: 5, firstName: 'Serena', location: 'San Francisco'}
-	   ]
+	{id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+        {id: 2, firstName: 'James', location: 'Columbia'},
+	{id: 5, firstName: 'Serena', location: 'San Francisco'}
+    ]
 }
